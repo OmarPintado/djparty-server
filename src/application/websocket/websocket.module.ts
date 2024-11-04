b5ac86bdb7f1c48d57d216a87b6dd3456f2af9d1
SGDIEGO <diegoalonsosg@gmail.com>
2024-11-04 14:39:28 -0500

    fix: updateing websocket

--- src/application/websocket/websocket.module.ts
+++ src/application/websocket/websocket.module.ts
@@ -3,10 +3,10 @@ import { WSGateway } from './websocket.gateway';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SongRequest } from 'src/domain/entities';
-import { SongRequestEvents } from './services/song-request.service';
+import { SongRequestServices } from './services/song-request.service';
 
 @Module({
     imports: [ConfigModule, TypeOrmModule.forFeature([SongRequest])],
-    providers: [WSGateway, SongRequestEvents],
+    providers: [WSGateway, SongRequestServices],
 })
 export class WebSocketModule {}
