a46e47a15b1f87835a91911f05ad7b9d022ce832
OmarPintado <omarpintado98@gmail.com>
2024-11-08 00:28:12 -0500

    feat: add validations for user in room, banned status, and active room state

--- src/application/song/song.module.ts
+++ src/application/song/song.module.ts
@@ -2,7 +2,14 @@ import { Module } from '@nestjs/common';
 import { SongService } from './song.service';
 import { SongController } from './song.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Artist, Song, SongArtists, SongRequest } from '../../domain/entities';
+import {
+    Artist,
+    RoomState,
+    Song,
+    SongArtists,
+    SongRequest,
+    UserMusicRoom,
+} from '../../domain/entities';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
@@ -10,7 +17,14 @@ import { AuthModule } from '../auth/auth.module';
     providers: [SongService],
     imports: [
         AuthModule,
-        TypeOrmModule.forFeature([Song, SongRequest, Artist, SongArtists]),
+        TypeOrmModule.forFeature([
+            Song,
+            SongRequest,
+            Artist,
+            SongArtists,
+            UserMusicRoom,
+            RoomState,
+        ]),
     ],
 })
 export class SongModule {}
--- src/application/song/song.service.ts
+++ src/application/song/song.service.ts
@@ -6,7 +6,14 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Artist, Song, SongArtists, SongRequest } from '../../domain/entities';
+import {
+    Artist,
+    RoomState,
+    Song,
+    SongArtists,
+    SongRequest,
+    UserMusicRoom,
+} from '../../domain/entities';
 import { SongRequestDto } from './dto/song-request.dto';
 
 @Injectable()
@@ -22,10 +29,22 @@ export class SongService {
         private readonly artistRepository: Repository<Artist>,
         @InjectRepository(SongArtists)
         private readonly songArtistsRepository: Repository<SongArtists>,
+        @InjectRepository(UserMusicRoom)
+        private readonly userMusicRoomRepository: Repository<UserMusicRoom>,
+        @InjectRepository(RoomState)
+        private readonly roomStateRepository: Repository<RoomState>,
     ) {}
 
     async sendSongRequest(songRequestDto: SongRequestDto) {
         try {
+            const { user_id, music_room_id } = songRequestDto;
+
+            // Verificar si la sala de música está activa
+            await this.validateRoomIsActive(music_room_id);
+
+            // Verificar si el usuario pertenece a la sala y que no esté baneado
+            await this.validateUserInRoomAndNotBanned(user_id, music_room_id);
+
             // Verificar si la canción ya está registrada en la base de datos
             let song = await this.songRepository.findOne({
                 where: { id_track: songRequestDto.spotify_track_id },
@@ -106,5 +125,41 @@ export class SongService {
                 'Hubo un error al procesar la solicitud de canción. Por favor, intenta nuevamente más tarde.',
             );
         }
+    }
+
+    private async validateUserInRoomAndNotBanned(
+        user_id: string,
+        music_room_id: string,
+    ) {
+        // Verificar si el usuario pertenece a la sala
+        const userMusicRoom = await this.userMusicRoomRepository.findOne({
+            where: { user_id, music_room_id },
+        });
+
+        if (!userMusicRoom) {
+            throw new BadRequestException(
+                'No estás dentro de esta sala de música.',
+            );
+        }
+
+        // Verificar si el usuario está baneado de la sala
+        if (userMusicRoom.is_banned) {
+            throw new BadRequestException('Fuiste baneado de esta sala. :(');
+        }
+
+        return true; // El usuario pertenece a la sala y no está baneado
+    }
+
+    private async validateRoomIsActive(music_room_id: string) {
+        // Verificar si la sala está activa (is_open = true)
+        const roomState = await this.roomStateRepository.findOne({
+            where: { music_room: { id: music_room_id } },
+        });
+
+        if (!roomState || !roomState.is_open) {
+            throw new BadRequestException('La sala de música no está activa.');
+        }
+
+        return true; // La sala está activa
     }
 }
