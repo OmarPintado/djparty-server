7f5b1f82ad3c326cde7c412a501030fe11cd090a
root <root@diego04>
2024-11-14 16:44:07 -0500

    feat: fixing music-room module

--- src/application/music-room/music-room.module.ts
+++ src/application/music-room/music-room.module.ts
@@ -5,11 +5,13 @@ import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MusicRoom, RoomState, UserMusicRoom } from '../../domain/entities';
 import { JoinToRoomService } from './join-to-room.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
         AuthModule,
         TypeOrmModule.forFeature([MusicRoom, RoomState, UserMusicRoom]),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
     ],
     controllers: [MusicRoomController],
     providers: [MusicRoomService, JoinToRoomService],
